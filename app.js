40d748e5cdbc412690e6e4294764dc3c9ef6455e
conan4069 <mevr02@gmail.com>
2020-05-31 15:21:08 -0400

    Se dejaron las funciones de filtrado por búsqueda y seccion independientes.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+// Product data
 const productos = [
     // {name: '', text: '', category: '', material: '', system: '', image:''},
-
     {
         name: 'Natural',
         text: 'Text Natural',
@@ -57,9 +57,12 @@ const productos = [
         system: 'handle',
         image: 'https://stosacucine30.imgix.net/en/immagini_cucine/classic-kitchens-saturnia-1824.jpg?auto=format%2Ccompress&crop=focalpoint&fit=crop&fp-x=.0&fp-y=.0&h=300&ixlib=php-2.3.0&mdate=1553702457&q=85&w=450'
     },
-
 ]
+// End - Product data
 
+
+
+// Categorys data
 const select_categorys = [
     'Modern',
     'Classic',
@@ -72,6 +75,9 @@ const select_categorys = [
 ]
 const content_category = document.querySelector('select[select-filter="categorys"]');
 
+
+
+// Materials data
 const select_materials = [
     'laminate',
     'veneer',
@@ -85,6 +91,9 @@ const select_materials = [
 ]
 const content_material = document.querySelector('select[select-filter="materials"]');
 
+
+
+// Systems data
 const select_systems = [
     'flat groove wint pocket',
     'Recessed groove',
@@ -96,6 +105,10 @@ const select_systems = [
 ] 
 const content_system = document.querySelector('select[select-filter="systems"]');
 
+
+
+
+// Select categorys List
 const contentCategory = () => {
 
     content_category.innerHTML = '';
@@ -110,6 +123,10 @@ const contentCategory = () => {
 }
 contentCategory();
 
+
+
+
+// Select Materials List
 const contentMaterial = () => {
 
     content_material.innerHTML = '<option value="*">Materials</option>';
@@ -124,6 +141,10 @@ const contentMaterial = () => {
 }
 contentMaterial();
 
+
+
+
+// Select Systems List
 const contentSystem = () => {
 
     content_system.innerHTML = '<option value="*">Opening systems</option>';
@@ -138,13 +159,18 @@ const contentSystem = () => {
 }
 contentSystem();
 
-const search_filter = document.querySelector('#search-filter');
-const results = document.querySelector('#products-filter');
 
+
+
+// Product content
 const contentProducts = () => {
+
+    // Container to list products
+    const results = document.querySelector('#products-filter');
 
     results.innerHTML = '';
 
+    // Dynamic structure of products
     for (let producto of productos) {
         let name = producto.name.toLowerCase();
 
@@ -171,16 +197,26 @@ const contentProducts = () => {
         `
     }
 
+    console.log(results.getElementsByClassName('categorys'))
+
     if (results.innerHTML === '') {
         results.innerHTML = `<div no-results style="display: flex">Any result... </div>`
     } else results.innerHTML += `<div no-results>Any result...</div>`
 }
 contentProducts();
 
+
+
+
+// Input Search
+const search_filter = document.querySelector('#search-filter');
+
 const filtrar = () => {
 
-    // Search length
+    // Button
+    const button_filter = document.querySelector('[btn-filter]');
 
+    // Search length
     if (document.getElementById('search-filter').value.length <= 0) {
         button_filter.setAttribute('deactivate','')
     }
@@ -191,7 +227,7 @@ const filtrar = () => {
 
     // console.log('texto',text)
 
-
+    // Hide or show
     for (let producto of productos) {
         let name = producto.name.toLowerCase();
 
@@ -203,11 +239,13 @@ const filtrar = () => {
         }
     }
 
+    // Check if there are products displayed
     const all_product = document.querySelectorAll('#products-filter [name]').length;
     // console.log('all_producta',all_product)
-    const d_none_product = document.querySelectorAll('#products-filter [name].d-none').length;
+    const d_none_product = document.querySelectorAll('#products-filter [name][class*="d-none"]').length;
     // console.log('d_none_product',d_none_product)
 
+    // If you don't find results
     if (all_product === d_none_product) {
         document.querySelector(`#products-filter > [no-results]`).style.display = 'flex';
     }
@@ -215,34 +253,36 @@ const filtrar = () => {
          document.querySelector(`#products-filter > [no-results]`).style.display = 'none';
     }
 }
+// Always active when writing to the input field
 search_filter.addEventListener('keyup', filtrar);
 
 
-const button_filter = document.querySelector('[btn-filter]');
 
+
+
+// Filter by select
 function selectFilter (argument) {
     // Filter the selects
 
-    // console.log('argument(',argument,')')
+    // Button
+    const button_filter = document.querySelector('[btn-filter]');
+
+    // Select value
     const value_select = document.querySelector(`[select-filter="${argument}"]`).value;
-    // console.log('value_select', value_select);
 
-    // console.log(button_filter)
-
+    // Select initial state (all)
     let normal_select = false;
 
-
+    // Botton on / off
     if (value_select === "*" && argument !== 'categorys') {
         normal_select = true;
-
         button_filter.setAttribute('deactivate','')
 
     }else{
-
         button_filter.removeAttribute('deactivate');
     }
 
-    // List of all products
+    // List of all products (select)
     const list_all_product_select = document.querySelectorAll(`[${argument}]`)
 
     list_all_product_select.forEach( function(product_select) {
@@ -260,12 +300,32 @@ function selectFilter (argument) {
 
     });
 
-}
 
+    // Check if there are products displayed
+    const all_product = document.querySelectorAll('#products-filter [name]').length;
+    const d_none_product = document.querySelectorAll('#products-filter [name][class*="d-none"]').length;
+
+    // If you don't find results
+    if (all_product === d_none_product) {
+        document.querySelector(`#products-filter > [no-results]`).style.display = 'flex';
+    }
+    else {
+        document.querySelector(`#products-filter > [no-results]`).style.display = 'none';
+    }
+
+}
+// The filter by selection is initialized in categories, to show initially
+selectFilter('categorys')
+
+
+
+// Clean filters by botton
 function resetFilters() {
 
-    // Reset input
+    //Clean select
     document.getElementById('search-filter').value ='';
+
+    // Reset input (search)
     const products_filter_none = document.querySelectorAll(`#products-filter .d-none`);
     products_filter_none.forEach(key => key.classList.remove('d-none'))
 
@@ -275,11 +335,14 @@ function resetFilters() {
     reset_select_filter.forEach( key => key.value = "*");
 
     // Reset all products
-    document.querySelectorAll('#products-filter .col-filter').forEach( key => key.classList.remove('d-none-materials', 'd-none-systems'))
+    document.querySelectorAll('#products-filter .col-filter').forEach( key => key.classList.remove('d-none-materials', 'd-none-systems'));
 
+    // Button
+    const button_filter = document.querySelector('[btn-filter]');
     button_filter.setAttribute('deactivate','')
+
+    // Reset the container of no results
+    document.querySelector(`#products-filter > [no-results]`).style.display = 'none';
+
 }
-
-selectFilter('categorys')
 resetFilters()
-
